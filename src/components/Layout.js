095d50aa186c0da1ef3d747613929642d1692f94
Jean Regisser <jean.regisser@gmail.com>
2019-02-26 09:58:00 +0100

    Add footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -44,11 +44,10 @@ const Layout = ({ children }) => {
             }
           >
             <main>{children}</main>
-            {/* <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer> */}
+            <footer>
+              © {new Date().getFullYear()} Jean Regisser, built with&nbsp;
+              <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </footer>
           </div>
         </>
       )}
